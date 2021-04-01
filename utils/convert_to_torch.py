414af8281eaaebbfbcd15fcd383818140b29116f
Yoo Seungwoo <dreamy1534@kaist.ac.kr>
2021-04-01 14:45:46 +0000

    feat(main): Refreshing gitignore

--- utils/convert_to_torch.py
+++ utils/convert_to_torch.py
@@ -1,4 +1,13 @@
+"""
+Script for converting TFRecord format to Pytorch compatible formats
+"""
+
 import os
 import sys
+from torch.utils.data import Dataset, DataLoader
 
-BASE_DIR = 
+import data_reader
+
+BASE_DIR = '../'    # path to project root directory
+sys.path.append(BASE_DIR)
+
