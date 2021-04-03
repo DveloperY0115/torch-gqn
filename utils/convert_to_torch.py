c135462cd1dcb6dc97e6b669d93a6face058870d
Yoo Seungwoo <dreamy1534@kaist.ac.kr>
2021-04-03 09:34:06 +0000

    feat(main): Add function _convert_frame_data

--- utils/convert_to_torch.py
+++ utils/convert_to_torch.py
@@ -4,10 +4,11 @@ Script for converting TFRecord format to Pytorch compatible formats
 
 import os
 import sys
-from torch.utils.data import Dataset, DataLoader
-
-import data_reader
+import tensorflow as tf
 
 BASE_DIR = '../'    # path to project root directory
 sys.path.append(BASE_DIR)
 
+def _convert_frame_data(jpeg_data):
+    decoded_frames = tf.image.decode_jpeg(jpeg_data)
+    return tf.image.convert_image_dtype(decoded_frames, dtype=tf.float32)
