4943377fa351483ca73a4a95bc80d8e8f180e184
Yoo Seungwoo <dreamy1534@kaist.ac.kr>
2021-04-03 09:34:56 +0000

    feat(main): Add named tuples

--- utils/convert_to_torch.py
+++ utils/convert_to_torch.py
@@ -9,6 +9,11 @@ import tensorflow as tf
 BASE_DIR = '../'    # path to project root directory
 sys.path.append(BASE_DIR)
 
+# define named tuple for data processing
+Context = collections.namedtuple('Context', ['frames', 'cameras'])
+Query = collections.namedtuple('Query', ['context', 'query_camera'])
+TaskData = collections.namedtuple('TaskData', ['query', 'target'])
+
 def _convert_frame_data(jpeg_data):
     decoded_frames = tf.image.decode_jpeg(jpeg_data)
     return tf.image.convert_image_dtype(decoded_frames, dtype=tf.float32)
