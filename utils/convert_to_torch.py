0e96438afccd9aa8f201858a228434e7a7202f19
Yoo Seungwoo <dreamy1534@kaist.ac.kr>
2021-04-03 12:37:23 +0000

    feat(main): Define overall routine of converter

--- utils/convert_to_torch.py
+++ utils/convert_to_torch.py
@@ -126,4 +126,54 @@ def make_context(frames, cameras):
 
 
 if __name__ == '__main__':
-    print(tf.__version__)
+    import sys
+
+    if len(sys.argv) < 3:
+        print('[!] Please specify a dataset to convert')
+        print('[!] python convert_to_torch.py dataset_name train/test/all')
+        print('[!] e.g. python convert_to_torch.py jaco train')
+        exit()
+
+    DATASET = sys.argv[1]
+    dataset_info = _DATASETS[DATASET]
+
+    converted_dataset_path = f'{DATASET}-torch'
+
+    # switches for converter
+    convert_train = False
+    convert_test = False
+
+    if sys.argv[2] == 'train':
+        convert_train = True
+        converted_dataset_train = f'{converted_dataset_path}/train'
+    
+    elif sys.argv[2] == 'test':
+        convert_test = True
+        converted_dataset_test = f'{converted_dataset_path}/test'
+    
+    elif sys.argv[2] == 'all':
+        convert_train = True
+        convert_test = True
+        converted_dataset_train = f'{converted_dataset_path}/train'
+        converted_dataset_test = f'{converted_dataset_path}/test'
+
+    else:
+        print('[!] Please provide valid argument')
+        exit()
+
+    # make directories for store data files
+    os.mkdir(converted_dataset_path)
+
+    if convert_train:
+        os.mkdir(converted_dataset_train)
+
+    if convert_test:
+        os.mkdir(converted_dataset_test)
+
+    # convert train dataset
+    if convert_train:
+        pass
+
+    # convert test datset
+    if convert_test:
+        pass
