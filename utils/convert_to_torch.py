af5986eb3c0cbf21079bd8bb7b1b6e2f0585ece3
Yoo Seungwoo <dreamy1534@kaist.ac.kr>
2021-04-03 09:41:38 +0000

    feat(main): Add named tuple DatasetInfo

--- utils/convert_to_torch.py
+++ utils/convert_to_torch.py
@@ -10,6 +10,12 @@ import tensorflow as tf
 BASE_DIR = '../'    # path to project root directory
 sys.path.append(BASE_DIR)
 
+# define structure for dataset metadata
+DatasetInfo = collections.namedtuple(
+    'DatasetInfo',
+    ['basepath', 'train_size', 'test_size', 'frame_size', 'sequence_size']
+)
+
 # define named tuple for data processing
 Context = collections.namedtuple('Context', ['frames', 'cameras'])
 Query = collections.namedtuple('Query', ['context', 'query_camera'])
@@ -18,6 +24,7 @@ TaskData = collections.namedtuple('TaskData', ['query', 'target'])
 def _convert_frame_data(jpeg_data):
     decoded_frames = tf.image.decode_jpeg(jpeg_data)
     return tf.image.convert_image_dtype(decoded_frames, dtype=tf.float32)
+
 
 if __name__ == '__main__':
     print(tf.__version__)
