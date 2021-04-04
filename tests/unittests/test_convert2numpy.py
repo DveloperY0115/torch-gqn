86a8225f995dc4d6fb4d4321847e4cfb4a56e63f
Yoo Seungwoo <dreamy1534@kaist.ac.kr>
2021-04-04 12:44:41 +0000

    refactor(main): Modify comments

--- tests/unittests/test_convert2numpy.py
+++ tests/unittests/test_convert2numpy.py
@@ -41,6 +41,9 @@ class convert2numpyTest(unittest.TestCase):
 
         frames, cameras = converter.convert_raw_to_numpy(raw_data, OUTPUT_DIR)
 
+        # converting single .tfrecord file
+        # images should be of size (10, 64, 64, 3)
+        # cameras should be of size (10, 5)
         self.assertTrue(frames.shape == (10, 64, 64, 3))
         self.assertTrue(cameras.shape == (10, 5))
 
