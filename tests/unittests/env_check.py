53ac13dcb1347e55a8231f1852ac7ca3c28afc52
Yoo Seungwoo <dreamy1534@kaist.ac.kr>
2021-04-08 07:09:18 +0000

    refactor(main): Improve code quality

--- tests/unittests/env_check.py
+++ tests/unittests/env_check.py
@@ -2,15 +2,13 @@
 Unit-test for installation validation
 """
 
-import os
-import sys
 import unittest
 
 import torch
 import tensorflow as tf
 
 class envTest(unittest.TestCase):
-    
+
     def test_runs(self):
         self.assertTrue(torch.__version__ == '1.8.1')    # should be 1.8.1
         self.assertTrue(tf.__version__ == '1.15.0')    # should be 1.15.0
