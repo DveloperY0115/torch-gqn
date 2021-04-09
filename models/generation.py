3fd1ad7091e25a7cfb84d7a36b5e6a855ee4302e
Yoo Seungwoo <dreamy1534@kaist.ac.kr>
2021-04-09 09:17:21 +0000

    feat(main): Define generation network architecture

---
+++ models/generation.py
@@ -0,0 +1,28 @@
+"""
+Generation network of GQN
+"""
+
+class GenerationCls(nn.Module):
+
+    def __init__(self):
+        """
+        Generation network consists of multiple ConvLSTM units
+        """
+
+        super(GenerationCls, self).__init__()
+
+    def forward(self, x):
+        pass
+
+
+class ConvLSTM(nn.Module):
+
+    def __init__(self):
+        """
+        Convolutional LSTM module for generation network
+        """
+
+        super(ConvLSTM, self).__init__()
+    
+    def forward(self, x):
+        pass
