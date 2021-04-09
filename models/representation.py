3e4032f772c58d725791e4fa17f56e61433c7c37
Yoo Seungwoo <dreamy1534@kaist.ac.kr>
2021-04-09 09:16:38 +0000

    feat(main): Define representation network architectures

---
+++ models/representation.py
@@ -0,0 +1,41 @@
+"""
+Representation network of GQN
+"""
+
+import torch
+
+class PyramidCls(nn.Module):
+
+    def __init__(self):
+        """
+        Pyramid architecture for scene representation
+        """
+
+        super(PyramidCls, self).__init__()
+
+    def forward(self, x):
+        pass
+
+class TowerCls(nn.Module):
+
+    def __init__(self):
+        """
+        Tower architecture for scene representation
+        """
+
+        super(TowerCls, self):__init__()
+
+    def forward(self, x):
+        pass
+
+class PoolCls(nn.Module):
+
+    def __init__(self):
+        """
+        Pool architecture for scene representation
+        """
+
+        super(PoolCls, self).__init__()
+
+    def forward(self, x):
+        pass
