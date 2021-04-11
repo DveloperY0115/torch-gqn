e00bf4cd510cd4d0a9925768b43f14d52d51c27d
Yoo Seungwoo <dreamy1534@kaist.ac.kr>
2021-04-11 10:26:32 +0000

    feat(main): [Incomplete] Implement ConvLSTMCls

--- models/conv_lstm.py
+++ models/conv_lstm.py
@@ -4,20 +4,6 @@ Generation network of GQN
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
-class GenerationCls(nn.Module):
-
-    def __init__(self):
-        """
-        Generation network consists of multiple ConvLSTM units
-        """
-
-        super(GenerationCls, self).__init__()
-
-    def forward(self, x):
-        pass
-
 
 class ConvLSTMCls(nn.Module):
 
@@ -31,13 +17,13 @@ class ConvLSTMCls(nn.Module):
         """
 
         super(ConvLSTMCls, self).__init__()
-        
+
         # Conv layers for each gate of LSTM cell (size preserving)
         self.forget_conv = nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=1, padding=2)
         self.input_conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=1, padding=2)
         self.input_conv_2 = nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=1, padding=2)
         self.output_conv = nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=1, padding=2)
-        self.skip_conv = nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=4, padding=)
+        self.skip_conv = nn.Conv2d(256, out_channels, kernel_size=4, stride=4)
     
     def forward(self, q, r, z, hidden_in, cell_in, skip_in):
         """
@@ -45,7 +31,7 @@ class ConvLSTMCls(nn.Module):
 
         Args:
         - q:
-        - r:
+        - r: A Tensor of shape (B, C, W, H). It contains scene representation
         - z:
         - hidden_in:
         - cell_in:
@@ -55,14 +41,14 @@ class ConvLSTMCls(nn.Module):
         """
         
         # concatenate inputs along C dimension
-        x = torch.cat([hidden_in, q, r, z], dim=1)
+        x = torch.cat((hidden_in, q, r, z), dim=1)
 
-        forget_gate = F.sigmoid(self.forget_conv(x))
-        input_gate = F.sigmoid(self.input_conv_1(x)) * F.tanh(self.input_conv_2(x))
-        output_gate = F.sigmoid(self.output_conv(x))
+        forget_gate = torch.sigmoid(self.forget_conv(x))
+        input_gate = torch.sigmoid(self.input_conv_1(x)) * F.tanh(self.input_conv_2(x))
+        output_gate = torch.sigmoid(self.output_conv(x))
 
         cell = forget_gate * cell_in + input_gate
-        hidden = output_gate * F.tanh(cell)
-        skip = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=4) + skip_in 
+        hidden = output_gate * torch.tanh(cell)
+        skip = self.skip_conv(hidden) + skip_in 
 
         return (hidden, cell, skip)
---
+++ tests/unittests/test_conv_lstm.py
@@ -0,0 +1,40 @@
+"""
+Unit test for generation architectures of GQN
+"""
+
+import os
+import sys
+import unittest
+from pathlib import Path
+
+import torch
+from torchsummary import summary
+
+# constants
+BASE_DIR = os.path.join(Path(__file__).parent.absolute(), '../../')    # path to project root directory
+
+sys.path.append(BASE_DIR)    # append project root to import paths
+
+from models.conv_lstm import ConvLSTMCls
+
+class ConvLSTMClsTest(unittest.TestCase):
+
+    def test_runs(self):
+
+        print('----------< Testing ConvLSTMCls >---------')
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        
+        model = ConvLSTMCls(256 * 2 + 128 + 7, 256).to(device)
+
+        # (dummy) input shapes for testing -> may behave differently in action
+        query_shape = (7, 16, 16)    # should the cell remember query then how should we modify it?
+        repr_shape = (256, 16, 16)
+        latent_shape = (128, 16, 16)
+        hidden_in_shape = (256, 16, 16)
+        cell_in_shape = (256, 16, 16)
+        skip_in_shape = (256, 4, 4)
+
+        summary(model, [query_shape, repr_shape, latent_shape, hidden_in_shape, cell_in_shape, skip_in_shape])
+
+if __name__ == '__main__':
+   unittest.main()
