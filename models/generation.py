ff6e2971a628d0379717ac189a26d84056ff224b
Yoo Seungwoo <dreamy1534@kaist.ac.kr>
2021-04-11 08:34:08 +0000

    feat(main): Add layers to ConvLSTMCls

--- models/generation.py
+++ models/generation.py
@@ -2,6 +2,10 @@
 Generation network of GQN
 """
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 class GenerationCls(nn.Module):
 
     def __init__(self):
@@ -15,14 +19,25 @@ class GenerationCls(nn.Module):
         pass
 
 
-class ConvLSTM(nn.Module):
+class ConvLSTMCls(nn.Module):
 
-    def __init__(self):
+    def __init__(self, in_channels, out_channels):
         """
-        Convolutional LSTM module for generation network
+        Convolutional LSTM block for generation network
+
+        Args:
+        - in_channels: Int. Number of channels of the input of Conv2D
+        - out_channels: Int. Number of channels of the result of Conv2D
         """
 
-        super(ConvLSTM, self).__init__()
+        super(ConvLSTMCls, self).__init__()
+        
+        # Conv layers for each gate of LSTM cell (size preserving)
+        self.forget_conv = nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=1, padding=2)
+        self.input_conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=1, padding=2)
+        self.input_conv_2 = nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=1, padding=2)
+        self.output_conv = nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=1, padding=2)
+
     
-    def forward(self, x):
+    def forward(self, q, r, c, h, z):
         pass
