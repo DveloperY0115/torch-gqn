4cf8f356f6afa2306d86cde873cce3638db46e9b
Yoo Seungwoo <dreamy1534@kaist.ac.kr>
2021-04-11 08:34:23 +0000

    feat(main): Basic structure for generation network unit-test

--- tests/unittests/test_generation.py
+++ tests/unittests/test_generation.py
@@ -1,3 +1,32 @@
 """
 Unit test for generation architectures of GQN
 """
+
+import os
+import sys
+import unittest
+from pathlib import Path
+
+import torch
+from torchsummary import summary
+
+# constants
+BASE_DIR = os.path.join(Path(__file__).parent.absolute(), '../../')    # path to project root directory
+
+sys.path.append(BASE_DIR)    # append project root to import paths
+
+from models.generation import ConvLSTM, GenerationCls
+
+class ConvLSTMClsTest(unittest.TestCase):
+
+    def test_runs(self):
+        pass
+
+class GenerationClsTest(unittest.TestCase):
+
+    def test_runs(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
