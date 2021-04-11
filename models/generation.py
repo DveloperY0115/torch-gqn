115df931ddfabe09e90693a9e33d36869f9f7957
Yoo Seungwoo <dreamy1534@kaist.ac.kr>
2021-04-11 09:26:42 +0000

    feat(main): Implement ConvLSTM 'forward'

--- models/generation.py
+++ models/generation.py
@@ -37,7 +37,32 @@ class ConvLSTMCls(nn.Module):
         self.input_conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=1, padding=2)
         self.input_conv_2 = nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=1, padding=2)
         self.output_conv = nn.Conv2d(in_channels, out_channels, kernel_size=5, stride=1, padding=2)
-
+        self.skip_conv = nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=4, padding=)
     
-    def forward(self, q, r, c, h, z):
-        pass
+    def forward(self, q, r, z, hidden_in, cell_in, skip_in):
+        """
+        Forward propagation
+
+        Args:
+        - q:
+        - r:
+        - z:
+        - hidden_in:
+        - cell_in:
+        - skip_in:
+
+        Returns: A tuple of Tensors. The tuple includes hidden state, output, and skip connection of the cell
+        """
+        
+        # concatenate inputs along C dimension
+        x = torch.cat([hidden_in, q, r, z], dim=1)
+
+        forget_gate = F.sigmoid(self.forget_conv(x))
+        input_gate = F.sigmoid(self.input_conv_1(x)) * F.tanh(self.input_conv_2(x))
+        output_gate = F.sigmoid(self.output_conv(x))
+
+        cell = forget_gate * cell_in + input_gate
+        hidden = output_gate * F.tanh(cell)
+        skip = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=4) + skip_in 
+
+        return (hidden, cell, skip)
