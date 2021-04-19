df541ca3aff28f48ad5e5044e4bc3be94cb0d235
Yoo Seungwoo <dreamy1534@kaist.ac.kr>
2021-04-19 13:19:44 +0000

    feat(main): Complete function documentation

--- models/conv_lstm.py
+++ models/conv_lstm.py
@@ -35,13 +35,16 @@ class ConvLSTMCls(nn.Module):
 
         Args:
         - q: A Tensor of shape (B, 7, 1, 1). Camera extrinsics of query view point
-        - r: A Tensor of shape (B, C, W, H). Contains scene representation
-        - z:
-        - hidden_in:
-        - cell_in:
-        - skip_in:
+        - r: A Tensor of shape (B, C, W, H). Contains scene representation. Usually of shape (B, 256, 16, 16) or (B, 256, 1, 1)
+        - z: A Tensor of shape (B, C, 16, 16). Upsampled latent vector from inference architecture
+        - hidden_in: A Tensor of shape (B, C, W, H). Hidden variable from previous ConvLSTM cell. Usually of shape (B, 256, 16, 16)
+        - cell_in: A Tensor of shape (B, C, W, H). Cell state from previous ConvLSTM cell. Usually of shape (B, 256, 16, 16)
+        - skip_in: A Tensor of shape (B, C, W, H). Skip connection from previous ConvLSTM cell. Usually of shape (B, 128, 64, 64)
 
-        Returns: A tuple of Tensors. The tuple includes hidden state, output, and skip connection of the cell
+        Returns: A tuple of Tensors.
+        - hidden: A Tensor of shape (B, C, W, H). Usually of shape (B, 256, 16, 16)
+        - cell: A Tensor of shape (B, C, W, H). Usually of shape (B, 256, 16, 16)
+        - skip: A Tensor of shape (B, C, W, H). Usually of shape (B, 128, 64, 64)
         """
         
         # concatenate inputs along C dimension
