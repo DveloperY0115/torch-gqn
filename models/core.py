6dcbfd0148bd365e56e518a18fbffdb720a72e61
Yoo SeungWoo <dreamy1534@gmail.com>
2021-05-01 18:18:15 +0900

    feat(main): Complete InferenceCore

--- models/core.py
+++ models/core.py
@@ -19,20 +19,20 @@ class GenerationCore(nn.Module):
 
         self.upsample_v_q = nn.ConvTranspose2d(7, 7, kernel_size=16, stride=16, padding=0, bias=False)
         self.upsample_r = nn.ConvTranspose2d(256, 256, kernel_size=16, stride=16, padding=0, bias=False)
-        self.core = ConvLSTMCls(7+256+3, 128, kernel_size=5, stride=1, padding=2)
+        self.core = ConvLSTMCls(7+256+128+128, 128, kernel_size=5, stride=1, padding=2)
         # used to generate skip connection in generation architecture
         self.upsample_h = nn.ConvTranspose2d(128, 128, kernel_size=4, stride=4, padding=0, bias=False)
-        
+
     def forward(self, v_q, r, z, hidden_in, cell_in, skip_in):
         """
         Forward propagation
 
-        Similar to that of ConvLSTM, but includes upsampling & downsampling of input data
+        Similar to that of ConvLSTM, but includes up-sampling & down-sampling of input data
 
         Args:
-        - q: A Tensor of shape (B, 7, 1, 1). Camera extrinsics of query view point
+        - v_q: A Tensor of shape (B, 7, 1, 1). Camera extrinsic of query view point
         - r: A Tensor of shape (B, C, W, H). Contains scene representation. Usually of shape (B, 256, 16, 16) or (B, 256, 1, 1)
-        - z: A Tensor of shape (B, C, 16, 16). Upsampled latent vector from inference architecture
+        - z: A Tensor of shape (B, C, 16, 16). Up-sampled latent vector from inference architecture
         - hidden_in: A Tensor of shape (B, C, W, H). Hidden variable from previous ConvLSTM cell. Usually of shape (B, 256, 16, 16)
         - cell_in: A Tensor of shape (B, C, W, H). Cell state from previous ConvLSTM cell. Usually of shape (B, 256, 16, 16)
         - skip_in: A Tensor of shape (B, C, W, H). Skip connection from previous ConvLSTM cell. Usually of shape (B, 128, 64, 64)
@@ -49,7 +49,7 @@ class GenerationCore(nn.Module):
         if r.size(2) == 1:    # (B, 256, 1, 1) -> (B, 256, 16, 16)
             r = self.upsample_r(r)    # if 'Pyramid' or 'Pool' architecture, up-sample the images
 
-        cell_g, hidden_g = self.core(torch.cat((v_q, r, z),  dim=1), (hidden_in, cell_in))
+        hidden_g, cell_g = self.core(torch.cat((v_q, r, z),  dim=1), (hidden_in, cell_in))
         skip_g = self.upsample_h(hidden_g) + skip_in
 
         return hidden_g, cell_g, skip_g
@@ -64,7 +64,28 @@ class InferenceCore(nn.Module):
 
         super(InferenceCore, self).__init__()
 
-        self.upsample_q = nn.ConvTranspose2d(7, 7, kernel_size=16, stride=16, padding=0, bias=False)
-    
-    def forward(self, x):
-        pass
+        self.downsample_x_q = nn.Conv2d(3, 3, kernel_size=4, stride=4, padding=0, bias=False)
+        self.upsample_v_q = nn.ConvTranspose2d(7, 7, kernel_size=16, stride=16, padding=0, bias=False)
+        self.upsample_r = nn.ConvTranspose2d(256, 256, kernel_size=16, stride=16, padding=0, bias=False)
+        self.core = ConvLSTMCls(7+3+256+128+128, 128, kernel_size=5, stride=1, padding=2)
+
+    def forward(self, v_q, x_q, r, hidden_g, hidden_in, cell_in):
+        """
+        Forward propagation
+
+        Similar to GenerationCore but without skip connection between cells
+
+        Args:
+            -
+        """
+
+        # up-sample or down-sample data if needed
+        v_q = self.upsample_v_q(v_q)    # (B, 7, 1, 1) -> (B, 7, 16, 16)
+        x_q = self.downsample_x_q(x_q)    # (B, 3, 64, 64) -> (B, 3, 16, 16)
+
+        if r.size(2) == 1:  # (B, 256, 1, 1) -> (B, 256, 16, 16)
+            r = self.upsample_r(r)  # if 'Pyramid' or 'Pool' architecture, up-sample the images
+
+        hidden_e, cell_e = self.core(torch.cat((hidden_g, x_q, v_q, r), dim=1), (hidden_in, cell_in))
+
+        return hidden_e, cell_e
