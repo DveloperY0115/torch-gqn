e541fd0cbe32e8925001b2414db1836054269236
Yoo Seungwoo <dreamy1534@kaist.ac.kr>
2021-05-10 07:14:45 +0000

    feat(main): Add method state_dict in scheduler

--- utils/scheduler.py
+++ utils/scheduler.py
@@ -10,5 +10,9 @@ class AnnealingStepLR(_LRScheduler):
         self.last_epoch = last_epoch
         super(AnnealingStepLR, self).__init__(optimizer)
 
+    def state_dict(self):
+        state_dict = {key: value for key, value in self.__dict__.items() if key not in ('optimizer')}
+        return state_dict
+
     def get_lr(self):
         return [max(self.mu_f +(self.mu_i - self.mu_f) * (1.0 - self.last_epoch / self.n), self.mu_f) for base_lr in self.base_lrs]
